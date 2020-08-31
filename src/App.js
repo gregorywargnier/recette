60e4c27ef5d4a6588703b6de14cb70f7bf7a9943
greg <greg@drdata.io>
2020-08-31 15:16:32 +0200

    project base

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+// CSS
+import './App.css'
+
+//Header
+import Header from './components/Header'
+
+class App extends Component {
+  state = {
+    pseudo: this.props.match.params.pseudo
+  }
+
+  render () {
+    return (
+      <div className='box'>
+        <Header pseudo={this.state.pseudo} />
+        <div className='cards'>
+          <div className='card'>
+            <h2>Une Carte</h2>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
